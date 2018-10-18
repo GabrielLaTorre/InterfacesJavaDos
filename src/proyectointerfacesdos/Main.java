c9681780b20e8f3b68574487e1545f81142b2d4d
Gabe <lt.gabriel96@gmail.com>
2018-10-17 22:28:33 -0300

    Interfaces proporcionadas por Java

---
+++ src/proyectointerfacesdos/Main.java
@@ -0,0 +1,14 @@
+package proyectointerfacesdos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Menu menu1 = new Menu();
+        menu1.setBounds(0,0,300,200);
+        menu1.setVisible(true);
+        menu1.setResizable(false);
+        menu1.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/proyectointerfacesdos/Menu.java
@@ -0,0 +1,71 @@
+package proyectointerfacesdos;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Menu extends JFrame implements ActionListener{
+
+   private JMenuBar menubar;
+   private JMenu menu1, menu2, menu3;
+   private JMenuItem menuitem1, menuitem2, menuitem3, menuitem4, menuitem5;
+
+
+
+public Menu(){
+   setLayout(null);   
+   menubar = new JMenuBar();
+   setJMenuBar(menubar);
+
+   menu1 = new JMenu("Opciones");
+   menubar.add(menu1);
+
+
+   menu2 = new JMenu ("Tamaño de la ventana");
+   menu1.add(menu2);
+
+   menu3 = new JMenu ("Color de fondo");
+   menu1.add(menu3);
+
+
+   menuitem1 = new JMenuItem("300x200");
+   menu2.add(menuitem1);
+   menuitem1.addActionListener(this);
+
+   menuitem2 = new JMenuItem("640x480");
+   menu2.add(menuitem2);
+   menuitem2.addActionListener(this);
+
+   menuitem3 = new JMenuItem("Rojo");
+   menu3.add(menuitem3);
+   menuitem3.addActionListener(this);
+
+   menuitem4 = new JMenuItem("Verde");
+   menu3.add(menuitem4);
+   menuitem4.addActionListener(this);
+
+  menuitem5 = new JMenuItem("Azul");
+   menu3.add(menuitem5);
+   menuitem5.addActionListener(this);
+ }
+
+
+public void actionPerformed (ActionEvent e){
+   
+   if(e.getSource() == menuitem1){
+     setSize(300,200);
+   }
+   if(e.getSource() == menuitem2){
+     setSize(640,480);
+   }
+   if(e.getSource() == menuitem3){
+     getContentPane().setBackground(new Color(255,0,0));
+   }
+   if(e.getSource() == menuitem4){
+     getContentPane().setBackground(new Color(0,255,0));
+   } 
+   if(e.getSource() == menuitem5){
+     getContentPane().setBackground(new Color(0,0,255));
+   } 
+ 
+}
+}
